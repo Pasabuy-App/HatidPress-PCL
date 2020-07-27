d2489a3d8cd15ee3777f18c369075b21a0646ed8
BytesCrafter <bytescrafter@gmail.com>
2020-07-27 13:47:42 +0800

    Added git init files and VS class base project.

---
+++ BaseClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HatidPress
+{
+    public class BaseClass
+    {
+        /// <summary>
+        /// Primary base domain url.
+        /// </summary>
+        public static string BaseDomainUrl
+        {
+            get
+            {
+                return "http://localhost";
+            }
+        }
+    }
+}
