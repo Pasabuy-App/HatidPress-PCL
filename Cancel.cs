d7fa624a8634088f9966ee45e882f9bef59b6a12
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-25 20:08:16 +0800

    Added Deliveries/Cancel.

---
+++ Cancel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using HatidPress.Deliveries.Struck;
+
+namespace HatidPress
+{
+    public class Cancel
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Rider Cancel the Job Class.
+        /// </summary>
+        private static Cancel instance;
+        public static Cancel Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Cancel();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public Cancel()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Methods
+        public async void Job(string wp_id, string session_key, string odid, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("odid", odid);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/hatidpress/v1/deliveries/cancel", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
