6bad264d8a44951d4dbb9ab991ea313e1d1ff4d3
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-25 20:21:26 +0800

    Added Listing Delivery.

---
+++ Deliveries/ListingDel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using HatidPress.Deliveries.Struck;
+
+namespace HatidPress.Deliveries
+{
+    public class ListingDel
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Listing Delivery Class.
+        /// </summary>
+        private static ListingDel instance;
+        public static ListingDel Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ListingDel);
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public ListingDel()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Methods
+        public async void GetData(string wp_id, string session_key, string del_id, string odid, string vehicle, string date_stage, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("del_id", del_id);
+            dict.Add("odid", odid);
+            dict.Add("vehicle", vehicle);
+            dict.Add("date_stage", date_stage);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/hatidpress/v1/deliveries/listing", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
